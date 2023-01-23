edf12f94af58cf128bb8b6c63812e2852b4a3846
ilhom <ilhommamatqulov276@gmail.com>
2023-01-23 23:41:12 +0500

    find02_max_count.py

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,10 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    i=0 
+    max1=data[0]
+    while i<len(data):
+        if max1<data[i]:
+            max1=data[i]
+        i+=1
+    return data.count(max1)
