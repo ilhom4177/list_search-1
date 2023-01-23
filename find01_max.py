d9c698ce415f8f59908ec098ab06f7d2fa0f2b1f
ilhom <ilhommamatqulov276@gmail.com>
2023-01-23 23:39:41 +0500

    find01_max.py

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,11 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
+    i=0 
+    max1=data[0]
+    while i<len(data):
+        if max1<data[i]:
+            max1=data[i]
+        i+=1
+    return max1 
     
