27dc2c9d5d936d63cec06b7631c2dec8b124f493
ilhom <ilhommamatqulov276@gmail.com>
2023-01-23 23:44:38 +0500

    find04_max_index.py

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,10 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    i=0 
+    max1=data[0]
+    while i<len(data):
+        if max1<data[i]:
+            max1=data[i]
+        i+=1
+    return data.index(max1) 
