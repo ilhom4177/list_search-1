2dc70bf6f4cc747019034ec565a8e90c209c7403
ilhom <ilhommamatqulov276@gmail.com>
2023-01-25 11:54:42 +0500

    find05_max_even.py

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,10 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    i=0 
+    max1 = 0
+    while i < len(data):
+        if max1 < data[i] and data[i] % 2 == 1:
+            max1 = data[i]
+        i += 1
+    return max1 
