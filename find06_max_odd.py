713d72686f90dda2dc1d0e429629060cd23d32df
ilhom <ilhommamatqulov276@gmail.com>
2023-01-25 11:56:34 +0500

    find06_max_odd.py

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,9 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    n = data[0]
+    while i < len(data):
+        if n < data[i] and data[i] % 2 == 1:
+            n = data[i]
+        i += 1
+    return n
