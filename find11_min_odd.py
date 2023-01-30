2f04a77011c1c179cdc0c594453e7e6c97d97a2b
ilhom <ilhommamatqulov276@gmail.com>
2023-01-30 14:19:50 +0500

    find11_min_odd.py

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -6,6 +6,6 @@ def find_min_odd(data):
     returns: minimum odd number in the list
     """
     evens = [num for num in data if num % 2 == 1]
-    return min(evens) if evens else None
+    return min(evens) if evens else -1
 
 print(find_min_odd([1,23,42,21,11,33]))
