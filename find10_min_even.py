7bf2d63ff35445f581e81457244460347cbe001c
ilhom <ilhommamatqulov276@gmail.com>
2023-01-30 10:35:07 +0500

    find10_min_even.py

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,9 +5,6 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    min_even = float("inf")
-    for num in data:
-        if num % 2 == 0 and num < min_even:
-            min_even = num
-    return min_even if min_even != float("inf") else None
+    evens = [num for num in data if num % 2 == 0]
+    return min(evens) if evens else None
 print(find_min_even([1, 8, 2, 8, 5]))
